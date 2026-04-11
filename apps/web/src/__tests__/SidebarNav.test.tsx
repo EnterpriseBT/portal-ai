1d85885b16222a32c257198489ec1e4f17bb2499
Ben Turner <bbgrabbag@gmail.com>
2026-04-11 05:09:47 +0000

    refactor: remove unused React import from SidebarNav test file

--- apps/web/src/__tests__/SidebarNav.test.tsx
+++ apps/web/src/__tests__/SidebarNav.test.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { render, screen } from "./test-utils";
 import userEvent from "@testing-library/user-event";
 import { SidebarNavUI } from "../components/SidebarNav.component";
